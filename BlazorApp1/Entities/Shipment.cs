62959bed96859930c4d888fb4b1fbea07368a50a
Arsalan Rahmanian <arsalan@AccountingPrivate2.taksunpars.local>
2025-07-22 17:10:59 +0330

    fourth

--- BlazorApp1/Entities/Shipment.cs
+++ BlazorApp1/Entities/Shipment.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using BlazorApp1.Services;
+using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Reflection;
 
@@ -7,123 +8,34 @@ namespace BlazorApp1.Entities;
 
 public class Shipment
 {
-    private DateTime? _shipmentDateGregorian;
-    private string _shipmentDatePersian;
-    private string _persianWeekday;
     public long Id { get; set; }
-
-    public DateTime? ShipmentDateGregorian
-    {
-        get => _shipmentDateGregorian;
-        set
-        {
-            _shipmentDateGregorian = value;
-            _shipmentDatePersian = GregorianToPersian(value);
-            _persianWeekday = PersianWeekday(value);
-        }
-    }
-
-    public string ShipmentDatePersian
-    {
-        get => _shipmentDatePersian;
-        private set => _shipmentDatePersian = value;
-    }
-    public string Weekday
-    {
-        get => _persianWeekday;
-        private set => _persianWeekday = value;
-    }
-
+    public DateTime ShipmentDateGregorian { get; set; }
+    public string ShipmentDatePersian { get; set; }
+    public string Weekday { get; set; } 
     public int? DriverPersonnelCode { get; set; }
     public int? DistributorPersonnelCode { get; set; }
     public string DriverName { get; set; }
-    public string DistributorName { get; set; }
+    public string DistributorName { get; set; } 
     public string RouteName { get; set; }
     public string WarehouseName { get; set; }
-    public int? InvoiceCount { get; set; } = 0;
-    public long? InvoiceAmount { get; set; } = 0;
-    public ICollection<ShipmentNumber> ShipmentNumbers { get; set; } = [];
-    public int? ReturnInvoiceCount { get; set; } = 0;
-    public long? ReturnInvoiceAmount { get; set; } = 0;
-    public int? SecondServiceInvoiceCount { get; set; } = 0;
-    public int? ThirdServiceInvoiceCount { get; set; } = 0;
-    public long? SecondServiceInvoiceAmount { get; set; } = 0;
-    public long? ThirdServiceInvoiceAmount { get; set; } = 0;
+    public int InvoiceCount { get; set; } = 0;
+    public long InvoiceAmount { get; set; } = 0;
+    public int ReturnInvoiceCount { get; set; } = 0;
+    public long ReturnInvoiceAmount { get; set; } = 0;
+    public int SecondServiceInvoiceCount { get; set; } = 0;
+    public int ThirdServiceInvoiceCount { get; set; } = 0;
+    public long SecondServiceInvoiceAmount { get; set; } = 0;
+    public long ThirdServiceInvoiceAmount { get; set; } = 0;
+    public bool HasVip { get; set; } = false;
+    public bool IsException { get; set; } = false;
 
     public int? NetInvoiceCount => InvoiceCount + SecondServiceInvoiceCount + ThirdServiceInvoiceCount - ReturnInvoiceCount;
     public long? NetInvoiceAmount => InvoiceAmount + SecondServiceInvoiceAmount + ThirdServiceInvoiceAmount - ReturnInvoiceAmount;
-
-    public bool HasVip { get; set; } = false;
-
-    public bool IsException { get; set; } = false;
 
     public Driver Driver { get; set; }
     public Route Route { get; set; }
     public Warehouse Warehouse { get; set; }
     public Distributor Distributor { get; set; }
 
-    private string PersianWeekday(DateTime? date)
-    {
-        if (date is null)
-            return "نا معتبر";
-        return date.Value.DayOfWeek switch
-        {
-            DayOfWeek.Saturday => "شنبه",
-            DayOfWeek.Sunday => "یکشنبه",
-            DayOfWeek.Monday => "دوشنبه",
-            DayOfWeek.Tuesday => "سه شنبه",
-            DayOfWeek.Wednesday => "چهارشنبه",
-            DayOfWeek.Thursday => "پنجشنبه",
-            DayOfWeek.Friday => "جمعه",
-            _ => "نا معتبر"
-        };
-    }
-    private string? GregorianToPersian(DateTime? date)
-    {
-        if (date is null) return null;
-        var pc = new PersianCalendar();
-        var d = date.Value;
-        return $"{pc.GetYear(d):0000}/{pc.GetMonth(d):00}/{pc.GetDayOfMonth(d):00}";
-    }
-
-
-
-
-    public bool Equals(Shipment? other)
-    {
-        if (other == null)
-            return false;
-
-        var properties = typeof(Shipment).GetProperties();
-
-        foreach (var prop in properties)
-        {
-            var thisValue = prop.GetValue(this);
-            var otherValue = prop.GetValue(other);
-            if (prop.Name == "ShipmentNumbers")
-            {
-                var thisNums = (thisValue as string).Split(',') ?? [];
-                var otherNums = (otherValue as string).Split(',') ?? [];
-
-                if (!thisNums.All(otherNums.Contains) || !otherNums.All(thisNums.Contains))
-                    return false;
-
-                continue;
-            }
-
-            if (thisValue == null && otherValue == null)
-                continue;
-
-
-
-            if (thisValue == null || otherValue == null)
-                return false;
-
-            if (!thisValue.Equals(otherValue))
-                return false;
-        }
-
-        return true;
-    }
-
+    public ICollection<ShipmentNumber> ShipmentNumbers { get; set; } = [];
 }
--- BlazorApp1/Services/ShipmentServices.cs
+++ BlazorApp1/Services/ShipmentServices.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using BlazorApp1.Entities;
+using System.Globalization;
 
 namespace BlazorApp1.Services;
 
@@ -9,4 +10,69 @@ public class ShipmentServices
         var pc = new PersianCalendar();
         return $"{pc.GetYear(myDate)}{delimiter}{pc.GetMonth(myDate):00}{delimiter}{pc.GetDayOfMonth(myDate):00}";
     }
+
+    public string PersianWeekday(DateTime? date)
+    {
+        if (date is null)
+            return "نا معتبر";
+        return date.Value.DayOfWeek switch
+        {
+            DayOfWeek.Saturday => "شنبه",
+            DayOfWeek.Sunday => "یکشنبه",
+            DayOfWeek.Monday => "دوشنبه",
+            DayOfWeek.Tuesday => "سه شنبه",
+            DayOfWeek.Wednesday => "چهارشنبه",
+            DayOfWeek.Thursday => "پنجشنبه",
+            DayOfWeek.Friday => "جمعه",
+            _ => "نا معتبر"
+        };
+    }
+
+    public static string? GregorianToPersian(DateTime? date)
+    {
+        if (date is null) return null;
+        var pc = new PersianCalendar();
+        var d = date.Value;
+        return $"{pc.GetYear(d):0000}/{pc.GetMonth(d):00}/{pc.GetDayOfMonth(d):00}";
+    }
+
+
+
+
+    //public static bool Equals(Shipment? other)
+    //{
+    //    if (other == null)
+    //        return false;
+
+    //    var properties = typeof(Shipment).GetProperties();
+
+    //    foreach (var prop in properties)
+    //    {
+    //        var thisValue = prop.GetValue(this);
+    //        var otherValue = prop.GetValue(other);
+    //        if (prop.Name == "ShipmentNumbers")
+    //        {
+    //            var thisNums = (thisValue as string).Split(',') ?? [];
+    //            var otherNums = (otherValue as string).Split(',') ?? [];
+
+    //            if (!thisNums.All(otherNums.Contains) || !otherNums.All(thisNums.Contains))
+    //                return false;
+
+    //            continue;
+    //        }
+
+    //        if (thisValue == null && otherValue == null)
+    //            continue;
+
+
+
+    //        if (thisValue == null || otherValue == null)
+    //            return false;
+
+    //        if (!thisValue.Equals(otherValue))
+    //            return false;
+    //    }
+
+    //    return true;
+    //}
 }
