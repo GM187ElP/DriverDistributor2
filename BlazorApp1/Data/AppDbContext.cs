29df002d3ace9ecd92b5edf62cb7e3356ee2ce2e
Arsalan Rahmanian <arsalan@AccountingPrivate2.taksunpars.local>
2025-07-31 14:17:44 +0330

    add user related classes

--- BlazorApp1/Data/AppDbContext.cs
+++ BlazorApp1/Data/AppDbContext.cs
@@ -15,4 +15,21 @@ public class AppDbContext : DbContext
     public DbSet<ShipmentNumber> ShipmentNumbers { get; set; }
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>().ToTable("Users", "Identity");
+        modelBuilder.Entity<Role>().ToTable("Roles", "Identity");
+        modelBuilder.Entity<UserRole>().ToTable("UserRoles", "Identity");
+        modelBuilder.Entity<UserClaim>().ToTable("UserClaims", "Identity");
+        modelBuilder.Entity<RoleClaim>().ToTable("RoleClaims", "Identity");
+
+        modelBuilder.Entity<UserRole>()
+            .HasKey(ur => new { ur.UserId, ur.RoleId });
+    }
 }
+
+
---
+++ BlazorApp1/Entities/User.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorApp1.Entities;
+
+public interface SoftDelete
+{
+    public bool IsDeleted { get; set; }
+}
+
+public class User : SoftDelete
+{
+    public int Id { get; set; }
+    public int Username { get; set; }
+    public bool IsDeleted { get; set; } = false;
+    public string PasswordHash { get; set; }
+    [NotMapped]
+    public bool IsPersistence { get; set; }
+    public ICollection<UserRole> UserRoles { get; set; }
+    public ICollection<UserClaim> UserClaims { get; set; }
+}
+
+public class Role : SoftDelete
+{
+    private string role;
+    public int Id { get; set; }
+    public string Name { get => role; set { role = value.ToUpper(); } }
+    public bool IsDeleted { get; set; }
+    public ICollection<UserRole> UserRoles { get; set; }
+    public ICollection<RoleClaim> RoleClaims { get; set; }
+}
+
+public class UserRole
+{
+    [Key]
+    public int UserId { get; set; }
+    [Key]
+    public int RoleId { get; set; }
+    public User User { get; set; }
+    public Role Role { get; set; }
+}
+
+public class UserClaim
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string ClaimId { get; set; }
+    public User User { get; set; }
+    public Claim Claim { get; set; }
+}
+
+public class RoleClaim
+{
+    public int Id { get; set; }
+    public int RoleId { get; set; }
+    public string ClaimType { get; set; }
+    public string ClaimValue { get; set; }
+    public Role Role { get; set; }
+    public Claim Claim { get; set; }
+}
+
