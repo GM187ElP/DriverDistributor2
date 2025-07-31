174300669bf0d007c1b68bd5da6183849ac6c9c9
Arsalan Rahmanian <arsalan@AccountingPrivate2.taksunpars.local>
2025-07-31 14:26:06 +0330

    add user related classes2

--- BlazorApp1/Entities/User.cs
+++ BlazorApp1/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Claims;
 
 namespace BlazorApp1.Entities;
 
@@ -23,39 +24,46 @@ public class User : SoftDelete
 public class Role : SoftDelete
 {
     private string role;
+
     public int Id { get; set; }
-    public string Name { get => role; set { role = value.ToUpper(); } }
+    public string Name { get => role; set { role = value?.ToUpper(); } }
     public bool IsDeleted { get; set; }
-    public ICollection<UserRole> UserRoles { get; set; }
-    public ICollection<RoleClaim> RoleClaims { get; set; }
+    public ICollection<UserRole> UserRoles { get; set; } = new HashSet<UserRole>();
+    public ICollection<RoleClaim> RoleClaims { get; set; } = new HashSet<RoleClaim>();
 }
 
-public class UserRole
+public class UserRole : SoftDelete
 {
-    [Key]
     public int UserId { get; set; }
-    [Key]
     public int RoleId { get; set; }
     public User User { get; set; }
     public Role Role { get; set; }
+    public bool IsDeleted { get; set; }
 }
 
-public class UserClaim
+public class UserClaim : SoftDelete
 {
+    private string claimType;
+    private string claimValue;
+
     public int Id { get; set; }
     public int UserId { get; set; }
-    public string ClaimId { get; set; }
+    public string ClaimType { get => claimType; set { claimType = value?.ToUpper(); } }
+    public string ClaimValue { get => claimValue; set { claimValue = value?.ToUpper(); } }
+    public bool IsDeleted { get; set; }
     public User User { get; set; }
-    public Claim Claim { get; set; }
 }
 
-public class RoleClaim
+public class RoleClaim : SoftDelete
 {
+    private string claimType;
+    private string claimValue;
+
     public int Id { get; set; }
     public int RoleId { get; set; }
-    public string ClaimType { get; set; }
-    public string ClaimValue { get; set; }
+    public string ClaimType { get => claimType; set { claimType = value?.ToUpper(); } }
+    public string ClaimValue { get => claimValue; set { claimValue = value?.ToUpper(); } }
+    public bool IsDeleted { get; set; }
     public Role Role { get; set; }
-    public Claim Claim { get; set; }
 }
 
